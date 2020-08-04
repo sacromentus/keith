d78b07e5768c812bfaf526d9a99256edf014487b
sacromentus <31938495+sacromentus@users.noreply.github.com>
2020-08-04 17:21:34 -0400

    Add files via upload

---
+++ services.js
@@ -0,0 +1,48 @@
+$("h4.bonding-header").on("click", function() {
+  $(".bonding-div").slideToggle();
+});
+
+$("h4.crown-header").on("click", function() {
+  $(".crown-div").slideToggle();
+});
+
+$("h4.whitening-header").on("click", function() {
+  $(".whitening-div").slideToggle();
+});
+
+$("h4.veneers-header").on("click", function() {
+  $(".veneers-div").slideToggle();
+});
+
+$("h4.implants-header").on("click", function() {
+  $(".implants-div").slideToggle();
+});
+
+
+$("h4.dentures-header").on("click", function() {
+  $(".dentures-div").slideToggle();
+});
+
+$("h4.roots-header").on("click", function() {
+  $(".roots-div").slideToggle();
+});
+
+$("h4.extractions-header").on("click", function() {
+  $(".extractions-div").slideToggle();
+});
+
+$("h4.hygiene-header").on("click", function() {
+  $(".hygiene-div").slideToggle();
+});
+
+$("h4.inlays-header").on("click", function() {
+  $(".inlays-div").slideToggle();
+});
+
+$("h4.crown-lengthening-header").on("click", function() {
+  $(".crown-lengthening-div").slideToggle();
+});
+
+$("h4.post-op-header").on("click", function() {
+  $(".post-op-div").slideToggle();
+});
